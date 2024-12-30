afd3219ac02198d1a368c0d8b63a14a2bc1e8c1f
shailaja <donthulashailaja@gmail.com>
2024-12-30 22:02:09 +0530

    First Commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+   facebook:string="facebook.png"
+   insta:string="insta.jpg"
+   linkedin:string="linkedin.png"
+   telegram:string="telegram.jpg"
+   whatsapp:string="whatsapp.jpg"
+   youtube:string="youtube.png"
+   twitter:string="x-twitter.svg"
+
+}
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+ main:string="main.png";
+ ecosystem:string="ecosystem.png"
+ presslogos:string="press-logos.png";
+ zero:string="zero.svg"
+ twenty:string="twenty.svg"
+}
